66b94f14ed79d7b1b52da6a9e4708e623faa6e50
enjore <shawn630730@gmail.com>
2025-05-21 11:47:07 +0700

    수정

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
@@ -43,12 +44,21 @@ class UserListView(ListView):
     template_name = 'user/user_list.html'
     context_object_name = 'users'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q = self.request.GET.get('q')
+
+        if q:
+            queryset = queryset.filter(
+                Q(title__icontains=q) |
+                Q(content__icontains=q)
+            )
+        return queryset
 
 # 사용자 상세페이지
 class UserDetailView(DetailView):
     model = User
     template_name = 'user/user_detail.html'
-    context_object_name = 'user'
     paginate_by = 10
 
 
@@ -58,4 +68,13 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserForm
     template_name = 'user/user_post.html'
-    success_url = reverse_lazy('user_list')  # 작성 후 리디렉션 위치
+
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy('user_list')
